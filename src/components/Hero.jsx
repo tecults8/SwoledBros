007faf4ea6e032ecc71f771231a7edfdeae36ecf
manibharathi11 <manibharathiinreallife@gmail.com>
2025-09-12 00:33:38 +0530

    React app init and basic router config

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div>
+      <h1>HERO_PAGE_TEST</h1>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/pages/TrainingPlanPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+
+function TrainingPlanPage() {
+  return (
+    <div>
+      <NavBar />
+      <h1>TEST_TRAINING_PLAN_PAGE</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default TrainingPlanPage;
